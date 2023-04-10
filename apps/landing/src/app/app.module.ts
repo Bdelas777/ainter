0886c142cca7db1ef0e8e4fdf70e850639f1f2c4
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-04-10 11:27:39 -0600

    Layout info

--- apps/landing/src/app/app.module.ts
+++ apps/landing/src/app/app.module.ts
@@ -3,22 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { ImagesComponent } from './components/images/images.component';
+
 import { DescriptionComponent } from './components/description/description.component';
 import { LevelComponent } from './components/level/level.component';
+//MOdulos
+import { LayoutModule } from './layout/layout.module';
+import { TopicsComponent } from './components/topics/topics.component';
+import { ImagesComponent } from './components/images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NxWelcomeComponent,
-    ImagesComponent,
     DescriptionComponent,
     LevelComponent,
+    TopicsComponent,
+    ImagesComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

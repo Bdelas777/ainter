1c2876894555d12592b280f8c99e20507c1521c3
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-04-09 20:04:39 -0600

    Se agregan componentes

--- apps/landing/src/app/app.module.ts
+++ apps/landing/src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { appRoutes } from "./app.routes";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { ImagesComponent } from './components/images/images.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { LevelComponent } from './components/level/level.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    ImagesComponent,
+    DescriptionComponent,
+    LevelComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
